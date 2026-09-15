76ba079d6eb67f3ca12727d0760277a4db7a3fff
Sohel Arman <sohelarman3232@gmail.com>
2026-09-16 03:29:58 +0600

    Refactor run method to use threading for actions

--- app/src/main/java/com/recordrepeat/bot/ReplayEngine.java
+++ app/src/main/java/com/recordrepeat/bot/ReplayEngine.java
@@ -1,40 +1,118 @@
 package com.recordrepeat.bot;
 
+import android.accessibilityservice.GestureDescription;
+import android.graphics.Path;
 import java.util.ArrayList;
+
 
 public class ReplayEngine {
 
+
     private boolean running = false;
+
+    private AutomationAccessibilityService service;
+
+
+    public ReplayEngine(
+            AutomationAccessibilityService service
+    ){
+
+        this.service = service;
+
+    }
+
 
 
     public void run(
             ArrayList<ActionStep> steps
-    ) {
+    ){
 
         running = true;
 
-        for(ActionStep step : steps){
 
-            if(!running){
-                break;
+        new Thread(() -> {
+
+
+            for(ActionStep step : steps){
+
+
+                if(!running){
+                    break;
+                }
+
+
+                try{
+
+                    Thread.sleep(1000);
+
+
+                    if(step.type.equals("CLICK")){
+
+
+                        String[] point =
+                                step.data.split(",");
+
+
+                        float x =
+                                Float.parseFloat(point[0]);
+
+
+                        float y =
+                                Float.parseFloat(point[1]);
+
+
+                        click(x,y);
+
+                    }
+
+
+                }catch(Exception e){
+
+                    e.printStackTrace();
+
+                }
+
             }
 
-            try {
 
-                Thread.sleep(1000);
+        }).start();
 
-                // Future:
-                // Click
-                // Type
-                // App switch
-
-            } catch(Exception e){
-
-                e.printStackTrace();
-
-            }
-        }
     }
+
+
+
+    private void click(
+            float x,
+            float y
+    ){
+
+        Path path = new Path();
+
+        path.moveTo(x,y);
+
+
+        GestureDescription.StrokeDescription stroke =
+                new GestureDescription.StrokeDescription(
+                        path,
+                        0,
+                        100
+                );
+
+
+        GestureDescription gesture =
+                new GestureDescription.Builder()
+                        .addStroke(stroke)
+                        .build();
+
+
+        service.dispatchGesture(
+                gesture,
+                null,
+                null
+        );
+
+    }
+
 
 
     public void stop(){
@@ -42,4 +120,5 @@ public class ReplayEngine {
         running = false;
 
     }
+
 }
