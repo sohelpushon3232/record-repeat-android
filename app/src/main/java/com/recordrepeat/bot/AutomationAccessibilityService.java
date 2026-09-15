7e1bb7c216f04bc36c6665e23907d8ccaa75a4ed
Sohel Arman <sohelarman3232@gmail.com>
2026-09-16 02:32:01 +0600

    Add toast notification on service connection
    Show a toast message when the service is connected.

--- app/src/main/java/com/recordrepeat/bot/AutomationAccessibilityService.java
+++ app/src/main/java/com/recordrepeat/bot/AutomationAccessibilityService.java
@@ -2,6 +2,7 @@ package com.recordrepeat.bot;
 
 import android.accessibilityservice.AccessibilityService;
 import android.view.accessibility.AccessibilityEvent;
+import android.widget.Toast;
 
 public class AutomationAccessibilityService extends AccessibilityService {
 
@@ -13,5 +14,16 @@ public class AutomationAccessibilityService extends AccessibilityService {
     @Override
     public void onInterrupt() {
 
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+
+        Toast.makeText(
+                this,
+                "Automation Service Started",
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
