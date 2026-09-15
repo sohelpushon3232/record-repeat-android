4ccf61b5ad4b3d7dd07f353fdd5db9d0af1cf3c4
Sohel Arman <sohelarman3232@gmail.com>
2026-09-16 03:27:05 +0600

    Update AutomationAccessibilityService.java

--- app/src/main/java/com/recordrepeat/bot/AutomationAccessibilityService.java
+++ app/src/main/java/com/recordrepeat/bot/AutomationAccessibilityService.java
@@ -6,24 +6,69 @@ import android.widget.Toast;
 
 public class AutomationAccessibilityService extends AccessibilityService {
 
+    private static AutomationAccessibilityService instance;
+
+    private RecordManager recordManager;
+
+
     @Override
-    public void onAccessibilityEvent(AccessibilityEvent event) {
+    protected void onServiceConnected() {
+
+        super.onServiceConnected();
+
+        instance = this;
+
+        recordManager = new RecordManager();
+
+        Toast.makeText(
+                this,
+                "Automation Service Ready",
+                Toast.LENGTH_SHORT
+        ).show();
+    }
+
+
+    @Override
+    public void onAccessibilityEvent(
+            AccessibilityEvent event
+    ) {
+
+        if(event == null){
+            return;
+        }
+
+
+        String text = "";
+
+        if(event.getText() != null){
+
+            text = event.getText().toString();
+
+        }
+
+
+        // Event save করার base
+        if(!text.isEmpty()){
+
+            recordManager.addStep(
+                    "TEXT",
+                    text
+            );
+
+        }
 
     }
+
 
     @Override
     public void onInterrupt() {
 
     }
 
-    @Override
-    protected void onServiceConnected() {
-        super.onServiceConnected();
 
-        Toast.makeText(
-                this,
-                "Automation Service Started",
-                Toast.LENGTH_SHORT
-        ).show();
+    public static AutomationAccessibilityService getInstance(){
+
+        return instance;
+
     }
 }
