a0490959f3381556ae5712b7b65da1884939329f
Sohel Arman <sohelarman3232@gmail.com>
2026-09-16 05:03:49 +0600

    Update ReplayEngine.java

--- app/src/main/java/com/recordrepeat/bot/ReplayEngine.java
+++ app/src/main/java/com/recordrepeat/bot/ReplayEngine.java
@@ -3,37 +3,112 @@ package com.recordrepeat.bot;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.util.List;
+
+
 public class ReplayEngine {
+
 
     private Context context;
 
-    public ReplayEngine(Context context) {
+
+    public ReplayEngine(Context context){
+
         this.context = context;
+
     }
 
 
-    public void start(int repeatCount) {
 
-        Toast.makeText(
-                context,
-                "Automation Running: " + repeatCount + " times",
-                Toast.LENGTH_SHORT
-        ).show();
+    public void start(
+            int repeatCount,
+            List<ActionStep> steps
+    ){
 
 
-        for(int i = 0; i < repeatCount; i++){
+        new Thread(() -> {
 
-            runSteps();
+
+            for(int i = 0; i < repeatCount; i++){
+
+
+                for(ActionStep step : steps){
+
+
+                    try{
+
+                        Thread.sleep(
+                                step.delay
+                        );
+
+
+                        execute(step);
+
+
+                    }catch(Exception e){
+
+                        e.printStackTrace();
+
+                    }
+
+                }
+
+            }
+
+
+            Toast.makeText(
+                    context,
+                    "Automation Finished",
+                    Toast.LENGTH_SHORT
+            ).show();
+
+
+        }).start();
+
+
+    }
+
+
+
+    private void execute(
+            ActionStep step
+    ){
+
+
+        if(step.action.equals("CLICK")){
+
+
+            System.out.println(
+                    "Click: " + step.text
+            );
+
 
         }
 
-    }
 
 
-    private void runSteps(){
+        if(step.action.equals("TEXT")){
 
-        // Future:
-        // saved ActionStep execute হবে এখানে
+
+            System.out.println(
+                    "Type: " + step.text
+            );
+
+
+        }
+
+
+
+        if(step.action.equals("VIEW")){
+
+
+            System.out.println(
+                    "View: " + step.text
+            );
+
+
+        }
+
 
     }
 
