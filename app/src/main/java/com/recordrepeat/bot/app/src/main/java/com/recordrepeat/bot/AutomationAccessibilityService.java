b8234e1b3a9cb3b67841a4a0f72d2a9b463eb72a
Sohel Arman <sohelarman3232@gmail.com>
2026-09-16 02:14:40 +0600

    Add AutomationAccessibilityService class

---
+++ app/src/main/java/com/recordrepeat/bot/app/src/main/java/com/recordrepeat/bot/AutomationAccessibilityService.java
@@ -0,0 +1,17 @@
+package com.recordrepeat.bot;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class AutomationAccessibilityService extends AccessibilityService {
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
