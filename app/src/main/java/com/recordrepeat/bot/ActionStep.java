dd1e85c1dc055f7f0e1b0d8bbc46ec9c24f2fc62
Sohel Arman <sohelarman3232@gmail.com>
2026-09-16 15:08:02 +0600

    Update ActionStep.java

--- app/src/main/java/com/recordrepeat/bot/ActionStep.java
+++ app/src/main/java/com/recordrepeat/bot/ActionStep.java
@@ -1,24 +1,179 @@
 package com.recordrepeat.bot;
 
+
+import org.json.JSONObject;
+
+
 public class ActionStep {
 
-    public String action;
-    public int x;
-    public int y;
-    public String text;
-    public long delay;
+
+    private String type;
+
+    private int x;
+
+    private int y;
+
+    private String value;
+
+    private long delay;
+
+
 
     public ActionStep(
-            String action,
+            String type,
             int x,
             int y,
-            String text,
+            String value,
             long delay
-    ) {
-        this.action = action;
+    ){
+
+        this.type = type;
+
         this.x = x;
+
         this.y = y;
-        this.text = text;
+
+        this.value = value;
+
         this.delay = delay;
+
     }
+
+
+
+
+
+    public String getType(){
+
+        return type;
+
+    }
+
+
+
+    public int getX(){
+
+        return x;
+
+    }
+
+
+
+    public int getY(){
+
+        return y;
+
+    }
+
+
+
+    public String getValue(){
+
+        return value;
+
+    }
+
+
+
+    public long getDelay(){
+
+        return delay;
+
+    }
+
+
+
+
+
+
+
+    public JSONObject toJSON(){
+
+
+        JSONObject obj =
+                new JSONObject();
+
+
+        try{
+
+
+            obj.put(
+                    "type",
+                    type
+            );
+
+
+            obj.put(
+                    "x",
+                    x
+            );
+
+
+            obj.put(
+                    "y",
+                    y
+            );
+
+
+            obj.put(
+                    "value",
+                    value
+            );
+
+
+            obj.put(
+                    "delay",
+                    delay
+            );
+
+
+        }catch(Exception ignored){}
+
+
+
+        return obj;
+
+
+    }
+
+
+
+
+
+
+
+    public static ActionStep fromJSON(
+            JSONObject obj
+    ){
+
+
+        try{
+
+
+            return new ActionStep(
+
+                    obj.getString("type"),
+
+                    obj.getInt("x"),
+
+                    obj.getInt("y"),
+
+                    obj.getString("value"),
+
+                    obj.getLong("delay")
+
+            );
+
+
+        }catch(Exception e){
+
+
+            return null;
+
+        }
+
+
+    }
+
+
 }
